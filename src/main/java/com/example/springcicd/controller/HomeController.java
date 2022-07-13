a1b9e20abccf34b10312caf4ef906f320660f8bf
mokhs00 <mokhs00@naver.com>
2022-07-13 20:09:06 +0900

    Add healthCheck api

---
+++ src/main/java/com/example/springcicd/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.springcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/health")
+    public void healthCheck() {
+    }
+}
