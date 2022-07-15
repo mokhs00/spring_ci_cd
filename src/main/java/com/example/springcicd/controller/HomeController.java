54c41559d7c903a90e2d0c48dd7209e407f67f25
mokhs00 <mokhs00@naver.com>
2022-07-15 23:46:02 +0900

    Add get env profile API

--- src/main/java/com/example/springcicd/controller/HomeController.java
+++ src/main/java/com/example/springcicd/controller/HomeController.java
@@ -1,12 +1,30 @@
 package com.example.springcicd.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Set;
+
+@RequiredArgsConstructor
 @RestController
 public class HomeController {
+    private final Environment env;
+    private final Set<String> envSet = Set.of("real1", "real2");
 
     @GetMapping("/health")
     public void healthCheck() {
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        String defaultEnv = "default";
+        
+        return Arrays.stream(env.getActiveProfiles())
+                .filter(envSet::contains)
+                .findAny()
+                .orElse(defaultEnv);
     }
 }
